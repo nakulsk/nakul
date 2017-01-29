911a5f42eafec4e436a38de2aa20c4cd2dec7665
Nakul K <kumbharenakul@gmail.com>
2017-01-29 21:12:44 +0530

    Revert "Version 2"
    This reverts commit d79f08b62c341d1c8ea677fbb8d19ea5791291ae.

--- Collaboration-Project/Version2/Backend/src/main/java/com/niit/backend/interfaces/FileUploadDAO.java
+++
@@ -1,9 +0,0 @@
-package com.niit.backend.interfaces;
-
-import com.niit.backend.model.UploadFile;
-
-public interface FileUploadDAO {
-	void save(UploadFile uploadFile);
-
-	UploadFile getFile(String username);
-}
